7b808fc0b5ce9be95d8961c8f1e5e24b933c6578
murattsenel <110337932+murattsenel@users.noreply.github.com>
2023-02-03 23:32:29 +0300

    Add files via upload

---
+++ arastirilacak örnekler/fonksiyonda_global_degisken_degisimi.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+// ben call by value da değişken değerlerini değiştiremeyiz sanıyordum.
+ancak arguman olarak gönderdiğimiz dışındaki global değişkenleri fonksiyonlar
+ hem kullanıp hemde değiştirebiliyormuş.
+
+#include <stdio.h>
+
+int g = 3;
+int y = 5;
+
+int f1(int x)
+{
+	y = 3;
+	return x * g++;
+}
+
+int f2(int x)
+{
+	return x * ++g;
+}
+
+
+int main()
+{
+	f1(3);
+	printf("%d", y);
+}
+
+
---
+++ arastirilacak örnekler/printf_x_++x.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+/*burada virgül operatör mü ayraç mı olarak kullanılmış
+çıktı legal mi yoksa tanımsız davranış mı*/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+	int x = 10;
+
+	printf("%d %d %d ", x, ++x, x++);
+
+
+	printf("%d %d %d ", x, x++,++x);
+
+
+}
